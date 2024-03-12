e9c94674d10482ac9a134554de33838a05b000ad
Matheus Nobrega <matheus.adao.nobrega@gmail.com>
2024-03-12 10:04:24 -0300

    add post component

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,18 +1,48 @@
-import { View } from "react-native";
+import { ScrollView } from "react-native";
 import Header from "../components/Header";
 import Stories from "../components/Stories";
+import Post from "../components/Post";
+import Fake from "../assets/fake-feminino.jpg";
+import PostImage from "../assets/post-example.png";
 
 export default function Home() {
   return (
-    <View
+    <ScrollView
+      showsVerticalScrollIndicator={false}
       style={{
-        flex: 1,
-        marginVertical: 32,
-        marginHorizontal: 16,
+        marginTop: 32,
+        marginHorizontal: 8,
       }}
     >
       <Header />
       <Stories />
-    </View>
+      <Post
+        profileImage={Fake}
+        postImage={PostImage}
+        profileName="Fulana da Silva"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Morbi porta, eros at luctus pretium, felis ex feugiat augue, quis mollis
+         magna risus a felis. Vivamus nunc neque, laoreet posuere scelerisque ac,
+          interdum nec dolor. Quisque ultricies tincidunt nisl, nec accumsan dolor sodales ut."
+      />
+      <Post
+        profileImage={Fake}
+        postImage={PostImage}
+        profileName="Fulana da Silva"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Morbi porta, eros at luctus pretium, felis ex feugiat augue, quis mollis
+         magna risus a felis. Vivamus nunc neque, laoreet posuere scelerisque ac,
+          interdum nec dolor. Quisque ultricies tincidunt nisl, nec accumsan dolor sodales ut."
+      />
+      <Post
+        profileImage={Fake}
+        postImage={PostImage}
+        profileName="Fulana da Silva"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Morbi porta, eros at luctus pretium, felis ex feugiat augue, quis mollis
+         magna risus a felis. Vivamus nunc neque, laoreet posuere scelerisque ac,
+          interdum nec dolor. Quisque ultricies tincidunt nisl, nec accumsan dolor sodales ut."
+      />
+    </ScrollView>
   );
 }
